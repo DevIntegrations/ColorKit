91c9ec258e229d7a76c2b56f78ae7e7cef156829
JamWils <jwils22@gmail.com>
2015-12-11 19:17:06 -0500

    Created new ColorKit embedded framework project, and first component ColorWheel

---
+++ ColorKit/ColorKit/ColorKit.h
@@ -0,0 +1,19 @@
+//
+//  ColorKit.h
+//  ColorKit
+//
+//  Created by James Wilson on 12/11/15.
+//  Copyright © 2015 Interactive Relativity LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorKit.
+FOUNDATION_EXPORT double ColorKitVersionNumber;
+
+//! Project version string for ColorKit.
+FOUNDATION_EXPORT const unsigned char ColorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorKit/PublicHeader.h>
+
+
---
+++ ColorKit/ColorKit/ColorWheel.swift
@@ -0,0 +1,9 @@
+//
+//  ColorWheel.swift
+//  ColorKit
+//
+//  Created by James Wilson on 12/11/15.
+//  Copyright © 2015 Interactive Relativity LLC. All rights reserved.
+//
+
+import Foundation
